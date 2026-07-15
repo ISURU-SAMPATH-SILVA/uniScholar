beae0e3414f689615d772f5a01f6243c78fad4e8
ISURU-SAMPYH-SILVA <info.isurusampathsilva@gmail.com>
2026-07-15 11:42:01 +0530

    🔨 adding Nav Bar and config it

---
+++ components/navbar.php
@@ -0,0 +1,26 @@
+<style>
+    
+</style>
+<nav class="main-navbar">
+    <div class="logo">
+        
+        <img src="img/Brand/Favicon-Black.svg" alt="uniScholar">
+    </div>
+    <div>
+        <h3>
+            uniScholar
+        </h3>
+        <h6>
+            The Ultimate University <br> Study Hub
+        </h6>
+    </div>
+    <ul class="nav-links">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="Explore.php">Explore</a></li>
+        <li><a href="Course.php">Course</a></li>
+        <li><a href="About.php">About</a></li>
+    </ul>
+    <div>
+        <a href="Login.php"  rel="noopener noreferrer">Login</a>
+    </div>
+</nav>
--- index.php
+++ index.php
@@ -2,19 +2,19 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="images/Logo-whit.svg">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-    <title>uniScholar-home</title>
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="img/Brand/Favicon.svg">
+  <title>uniScholar</title>
+
 </head>
 
 <body>
+  <!--  Navbar Component  -->
+    <?php require 'components/navbar.php'; ?>
 
-<button class="btn btn-primary" id="myButton">Click Me</button>
-<script src="App.js"></script> 
+  <h1>uniScholar</h1>
+
 </body>
 
 </html>
