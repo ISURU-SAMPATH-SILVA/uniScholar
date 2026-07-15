c24c28617db38d4f20ddd2f1c8c2ed7a2f66b0c9
ISURU-SAMPYH-SILVA <info.isurusampathsilva@gmail.com>
2026-07-15 19:02:32 +0530

    🔨 Footer fille add and config it

---
+++ components/Footer.php
@@ -0,0 +1,42 @@
+<style>
+.main-footer {
+    background-color:var(--color-body);
+    color:var(--color-primry);
+    padding: 20px 0;
+    text-align: center;
+}
+</style>
+<footer class="main-footer container">
+   <div>
+    <img src="img/Brand/Favicon-White.svg" alt="uniScholar-logo">
+   </div>
+   <div><h2>Quick Links</h2>
+       <ul>
+           <li><a href="index.php">Home</a></li>
+           <li><a href="Explore.php">Explore</a></li>
+           <li><a href="Course.php">Course</a></li>
+           <li><a href="About.php">About</a></li>
+       </ul>
+       <ul>
+           <li><a href="GPA-cal.php">GPA Calculator</a></li>
+           <li><a href="Nots.php">Notes</a></li>
+           <li><a href="PAst-papers.php">Past Papers</a></li>
+           <li><a href="Help.php">Need Help</a></li>
+           <li><a href="Search.php">Search</a></li>
+       </ul>
+   </div>
+   <div>
+         <h2>Contact</h2>
+         <p>Address:Rajarata University of Sri Lanka,Mihintale.</p>
+         <p>Phone: +94 784901874</p> 
+         <p>Email: <a href="mailto:info@unischolar.com">info@unischolar.com</a></p>
+            
+   </div>
+   <div>
+    <h2>About</h2>
+    <p>public University located in the historic city of Mihintale, near Anuradhapura, Sri Lanka. The Rajarata University of Sri Lanka was established as the eleventh University in Sri Lanka and was opened on January 31st, 1996.</p>
+   </div>
+   <div>
+    <p>&copy;  Footer: © 2026 uniScholar |<p>Email: <a href="mailto:info@unischolar.com">info@unischolar.com</a></p></p>
+   </div>
+</footer>
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="icon" type="image/png" href="img/Brand/Favicon.svg">
   <title>uniScholar</title>
+  <link rel="stylesheet" href="css/style.css">
 
 </head>
 
@@ -14,7 +15,8 @@
     <?php require 'components/navbar.php'; ?>
 
   <h1>uniScholar</h1>
-
+<!--  Footer Component  -->
+    <?php require 'components/Footer.php'; ?>
 </body>
 
 </html>
