30d73dfd4a326cbdd64ce74ee3cd816212899af0
ISURU-SAMPYH-SILVA <info.isurusampathsilva@gmail.com>
2026-07-30 13:10:48 +0530

    ⚙️ config about page anad ading css

--- components/About.php
+++ components/About.php
@@ -14,30 +14,30 @@
 
 <body>
     <?php require 'navbar.php'; ?>
-    <div>
+    <div class="About_UniScholar">
         <h1>About UniScholar</h1>
         <h3>Discover Our Mission</h3>
         <p>UniScholar is a centralized, student-driven academic ecosystem developed specifically for higher education institutes. The platform serves a dual purpose: acting as a digital repository for high-quality academic
             resources and a comprehensive search engine for educational scholarships.</p>
     </div>
-    <div>
+    <div class="What_We_Offer">
 
         <h3>What We Offer</h3>
         <p>
             Empowering students with knowledge and financial opportunity </p>
     </div>
-    <div>
-        <h3>Resource Sharing</h3>
+    <div class="Resource_Sharing">
+        <h4>Resource Sharing</h4>
         <p>
 Seamlessly upload, archive, and download verified lecture notes, past examination papers, and tutorial sheets accurately categorized by course codes.</p>
     </div>
-      <div>
-        <h3>Scholarship Finder</h3>
+      <div class="Scholarship_Finder">
+        <h4>Scholarship Finder</h4>
         <p>
 Access a structured directory of local and global financial aid programs. Filter through available fields of study, degrees, and funding countries effortlessly.</p>
     </div>
-  <div>
-        <h3>Student Dashboard</h3>
+  <div class="Student_Dashboard">
+        <h4>Student Dashboard</h4>
         <p>
 Take full control of your academic logs. Manage your file uploads and monitor the live approval states (Pending, Approved, Rejected) of your tracking sheets.</p>
     </div>
